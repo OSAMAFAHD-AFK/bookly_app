c319c23822e07c091603d94c867af436ca738a8e
Osama <osama.87664@gmail.com>
2025-08-04 13:14:47 +0300

    refactor init satate & navigate to home

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly_app/Features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/Features/splash/presentation/views/widgets/sliding_text.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -16,26 +20,16 @@ class _SplashViewBodyState extends State<SplashViewBody>
 
   @override
   void initState() {
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1), // مدة الأنميشن
-    );
-
-    slidingAnimation = Tween<Offset>(
-      begin: const Offset(0, 2),
-      end: Offset.zero,
-    ).animate(animationController);
-
-    animationController.forward();
-    // تبدأ تشغيل الأنميشن (من البداية للنهاية)
-
     super.initState();
+    initSlidingAnimation();
+    navigateToHome();
   }
 
   @override
   void dispose() {
-    animationController.dispose();
     super.dispose();
+
+    animationController.dispose();
   }
 
   @override
@@ -50,6 +44,31 @@ class _SplashViewBodyState extends State<SplashViewBody>
         SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1), // مدة الأنميشن
+    );
+
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+    // تبدأ تشغيل الأنميشن (من البداية للنهاية)
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.fade,
+        duration: kTransitionDuration,
+      );
+    });
   }
 }
 
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xfff100b20);
+
+const kTransitionDuration = Duration(milliseconds: 250);
+// مدة الانتقال بين الصفحات
