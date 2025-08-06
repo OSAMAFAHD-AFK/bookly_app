ba3286bbfdfe7cc33f059d36c95b3cd7dc37cbeb
Osama <osama.87664@gmail.com>
2025-08-06 10:41:34 +0300

    add title & subtitle to book details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image_item.dart';
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/style.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -19,6 +20,27 @@ class BookDetailsViewBody extends StatelessWidget {
               horizontal: MediaQuery.of(context).size.width * .2,
             ),
             child: const CustomBookImageItem(),
+          ),
+          const SizedBox(height: 43),
+
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 6),
+
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(
+                fontStyle: FontStyle.italic,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ),
         ],
       ),
