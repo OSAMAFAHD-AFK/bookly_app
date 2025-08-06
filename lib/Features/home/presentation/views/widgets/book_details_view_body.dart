b33a8f550ab25b1c331d7b0cab8d3e0f723fce98
Osama <osama.87664@gmail.com>
2025-08-06 12:18:16 +0300

    create similar books list view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -2,6 +2,7 @@ import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.
 import 'package:bookly_app/Features/home/presentation/views/widgets/books_action.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_image_item.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/style.dart';
 import 'package:flutter/material.dart';
@@ -16,14 +17,14 @@ class BookDetailsViewBody extends StatelessWidget {
       child: Column(
         children: [
           const CustomBookDetailsAppBar(),
-          const SizedBox(height: 40),
+          const SizedBox(height: 30),
           Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: MediaQuery.of(context).size.width * .2,
+              horizontal: MediaQuery.of(context).size.width * .23,
             ),
             child: const CustomBookImageItem(),
           ),
-          const SizedBox(height: 43),
+          const SizedBox(height: 40),
           Text(
             'The Jungle Book',
             style: Styles.textStyle30.copyWith(
@@ -48,6 +49,19 @@ class BookDetailsViewBody extends StatelessWidget {
           ),
           const SizedBox(height: 37),
           const BooksAction(),
+          const SizedBox(height: 50),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle16.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const SimilarBooksListView(),
+          const SizedBox(height: 24),
         ],
       ),
     );
