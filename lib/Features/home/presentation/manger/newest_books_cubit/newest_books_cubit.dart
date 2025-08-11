1ba70faa58fb24d8b9d86056a97c188b86077476
Osama <osama.87664@gmail.com>
2025-08-11 19:42:19 +0300

    newest books cubit done

--- lib/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
+++ lib/Features/home/presentation/manger/newest_books_cubit/newest_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/Features/home/data/models/books_models/item.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
 }
